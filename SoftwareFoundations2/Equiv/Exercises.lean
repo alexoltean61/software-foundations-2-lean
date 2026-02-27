4ea171cfe831bcb40e13ec6bd8af22aadd41209b
Alex Oltean <aoltean@mailbox.org>
2026-02-27 15:50:54 +0200

    equiv: Definition & exercises

---
+++ SoftwareFoundations2/Equiv/Exercises.lean
@@ -0,0 +1,177 @@
+import SoftwareFoundations2.Equiv.Def
+
+open ComEval
+
+variable {c c₁ c₂ c₃ : Com}
+variable {b : BExp}
+
+theorem aequiv_example : aexp⟨{ x - x }⟩ ≃ aexp⟨{ 0 }⟩ := by
+  simp [aequiv, AExp.eval]
+
+theorem bequiv_example : bexp⟨{ x - x == 0 }⟩ ≃ bexp⟨{ btrue }⟩ := by
+  simp [bequiv, BExp.eval]
+
+theorem skip_left : ⟨{ skip; ↑c }⟩ ≃ ⟨{ ↑c }⟩ := by
+  -- WORKED IN CLASS
+  intro σ σ'
+  apply Iff.intro
+  · intro h
+    cases h with
+    | ESeq h1 h2 =>
+        cases h1
+        exact h2
+  · intro h
+    apply ESeq ESkip
+    exact h
+
+theorem true_if (h : b ≃ bexp⟨{ btrue }⟩) :
+  ⟨{ if ↑b then ↑c₁ else ↑c₂ endif }⟩ ≃ ⟨{ ↑c₁ }⟩ := by
+  intro σ σ'
+  apply Iff.intro
+  · intro h1
+    cases h1 with
+    | EIfTrue _ _ => assumption
+    | EIfFalse habs _ =>
+        simp only [bequiv, BExp.eval] at h
+        specialize h σ
+        rw [h] at habs
+        contradiction
+  · intro h1
+    apply EIfTrue _ h1
+    apply h
+
+theorem false_while (h : b ≃ bexp⟨{ bfalse }⟩) :
+  ⟨{ while ↑b do ↑c od }⟩ ≃ ⟨{ skip }⟩ := by
+  intro σ σ'
+  apply Iff.intro
+  · intro h1
+    cases h1 with
+    | EWhileFalse => exact ESkip
+    | EWhileTrue habs =>
+        simp only [bequiv, BExp.eval] at h
+        specialize h σ
+        rw [h] at habs
+        contradiction
+  · intro h2
+    cases h2
+    apply EWhileFalse
+    apply h
+
+/-- TODO: In Lean, this proof is way uglier than in Rocq.
+
+    The `induction` tactic can only be used on inductive families
+    with *variable*, not fixed, indices.
+    I.e., the conclusion may look like `¬ σ =[ loop ]=> σ'`,
+    but it **must not** look like `¬ σ =[ ⟨{ while ↑b do ↑c od }⟩ ]=> σ'`.
+    We also can't (easily) generate the induction hypothesis manually, because
+    we need to prove termination.
+
+    So we weaken the statement and introduce hypothesis `eq` which stipulates equality:
+      `loop = ⟨{ while ↑b do ↑c od }⟩`
+    And we prove `¬ σ =[ loop ]=> σ'`.
+
+    This is enough to allow the `induction` tactic to succeed, but the generated induction
+    hypotheses are not very intuitive (check `ih` in the `EWhileTrue` case).
+
+    `aesop` suffices to prove everything, but this proof is really not illuminating at all.
+-/
+theorem true_while_nonterm'
+  {loop : Com}
+  (h : b ≃ bexp⟨{ btrue }⟩)
+  (eq : loop = ⟨{ while ↑b do ↑c od }⟩) :
+  ¬ σ =[ loop ]=> σ' := by
+  intro habs
+  induction habs with
+  | EWhileFalse habs =>
+      aesop
+  | EWhileTrue htrue h1 h2 _ ih =>
+      exact ih eq
+  | _ => aesop
+
+/-- Read the docstring above.
+    Try to write this proof manually on paper.
+-/
+theorem true_while_nonterm
+  (h : b ≃ bexp⟨{ btrue }⟩) :
+  ¬ σ =[ while ↑b do ↑c od ]=> σ' := by
+  -- WORKED IN CLASS
+  apply true_while_nonterm' h rfl
+
+theorem loop_unrolling :
+  ⟨{  while ↑b do ↑c od  }⟩ ≃
+  ⟨{  if ↑b then
+        ↑c;
+        while ↑b do ↑c od;
+      else
+        skip;
+      endif
+  }⟩ := by
+  -- WORKED IN CLASS
+  intro σ σ'
+  apply Iff.intro
+  · intro h
+    cases h with
+    | EWhileTrue beval =>
+        apply EIfTrue beval
+        apply ESeq
+        repeat assumption
+    | EWhileFalse beval =>
+        apply EIfFalse beval
+        apply ESkip
+  · intro h
+    cases h with
+    | EIfTrue beval h =>
+        cases h
+        apply EWhileTrue beval
+        repeat assumption
+    | EIfFalse beval h =>
+        cases h
+        apply EWhileFalse beval
+
+theorem identity_assignment :
+  ⟨{ x = x }⟩ ≃ ⟨{ skip }⟩ := by
+  intro σ σ'
+  apply Iff.intro
+  · intro h
+    cases h
+    case EAsgn n eqn eqs
+    · subst eqn
+      simp only [AExp.eval, State.set_id] at eqs
+      subst eqs
+      exact ESkip
+  · intro h
+    cases h
+    apply EAsgn rfl
+    simp only [AExp.eval, State.set_id]
+
+theorem skip_right : ⟨{ ↑c; skip }⟩ ≃ ⟨{ ↑c }⟩ := by
+  -- FILL IN HERE
+  sorry
+
+theorem false_if (h : b ≃ bexp⟨{ bfalse }⟩) :
+  ⟨{ if ↑b then ↑c₁ else ↑c₂ endif }⟩ ≃ ⟨{ ↑c₂ }⟩ := by
+  -- FILL IN HERE
+  sorry
+
+theorem swap_if_branches :
+    ⟨{ if ↑b then ↑c₁ else ↑c₂ endif }⟩ ≃
+    ⟨{ if !↑b then ↑c₂ else ↑c₁ endif }⟩ := by
+  -- FILL IN HERE
+  sorry
+
+theorem true_while
+  (h : b ≃ bexp⟨{ btrue }⟩) :
+  ⟨{ while ↑b do ↑c od }⟩ ≃ ⟨{ while btrue do skip od }⟩ := by
+  -- FILL IN HERE
+  -- Hint: You'll want to use `true_while_nonterm` here.
+  sorry
+
+theorem seq_assoc : ⟨{ {↑c₁ ; ↑c₂} ; ↑c₃ }⟩ ≃ ⟨{ ↑c₁ ; {↑c₂ ; ↑c₃} }⟩ := by
+  -- FILL IN HERE (optional)
+  sorry
+
+theorem assign_aequiv
+  (h : aexp⟨{ x }⟩ ≃ a ) :
+  ⟨{ x = ↑a }⟩ ≃ ⟨{ skip }⟩ := by
+  -- FILL IN HERE
+  sorry
