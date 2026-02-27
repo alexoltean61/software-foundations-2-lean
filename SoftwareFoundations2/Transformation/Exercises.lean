b850677b4ceab87e023453a1ec32d9d7df1f8229
Alex Oltean <aoltean@mailbox.org>
2026-02-27 17:12:20 +0200

    transform: Beginning pgm transform, folding

---
+++ SoftwareFoundations2/Transformation/Exercises.lean
@@ -0,0 +1,11 @@
+import SoftwareFoundations2.Transformation.ConstantFolding
+
+theorem fold_constants_aexp_sound : AExp.fold_constants.sound := by
+  intro aexp σ
+  sorry
+
+theorem fold_constants_bexp_sound : BExp.fold_constants.sound := by
+  sorry
+
+theorem fold_constants_com_sound : Com.fold_constants.sound := by
+  sorry
