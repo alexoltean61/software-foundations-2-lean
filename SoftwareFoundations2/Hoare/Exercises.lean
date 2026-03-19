7e5ef36c23579b8a0b5edf1c90f9c25a47d99b65
Andrei Popa <andreipopa0498@gmail.com>
2026-03-19 11:52:58 +0200

    add solutions for Hoare

--- SoftwareFoundations2/Hoare/Exercises.lean
+++ SoftwareFoundations2/Hoare/Exercises.lean
@@ -27,14 +27,26 @@ open Hoare Proof
 def hoare_asgn_wrong : ∃ a, ¬ ⊨ ⦃ ⊤ ⦄ ⟨{ x = ↑a }⟩ ⦃ x = a ⦄ := by
   sorry
 
-lemma Assertion.impl_self : P ->> P :=
-  sorry
+lemma Assertion.impl_self : P ->> P := by
+  intros h
+  intros h1
+  assumption
 
-def Hoare.HPreStrengthen : Proof P' c Q → (P ->> P') → Proof P c Q :=
-  sorry
+def Hoare.HPreStrengthen : Proof P' c Q → (P ->> P') → Proof P c Q := by
+  intros h1
+  intros h2
+  apply HConsequence
+  · exact h1
+  · exact Assertion.impl_self
+  exact h2
 
-def Hoare.HPostWeaken : Proof P c Q' → (Q' ->> Q) → Proof P c Q :=
-  sorry
+def Hoare.HPostWeaken : Proof P c Q' → (Q' ->> Q) → Proof P c Q := by
+  intros h1
+  intros h2
+  apply HConsequence
+  · exact h1
+  · exact h2
+  exact Assertion.impl_self
 
 def swap {n m : ℕ} :
   ⊢ ⦃ x = n ∧ y = m ⦄
@@ -44,7 +56,13 @@ def swap {n m : ℕ} :
           x = x - y;
       }⟩
     ⦃ x = m ∧ y = n ⦄ := by
-  sorry
+  apply HSeq
+  · apply HSeq
+    · apply HAsgn
+    apply HAsgn
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
 
 def reduce_to_zero :
   ⊢ ⦃ ⊤ ⦄
@@ -54,7 +72,14 @@ def reduce_to_zero :
           od
       }⟩
     ⦃ x = 0 ⦄ := by
-  sorry
+  apply HConsequence
+  · apply HWhile ⦃ T ⦄
+    apply HPreStrengthen
+    · apply HAsgn
+    verify_assertion
+  · verify_assertion
+  apply Assertion.impl_self
+
 
 def if_minus_plus_dec :
   ⊢ ⦃ ⊤ ⦄
@@ -66,7 +91,13 @@ def if_minus_plus_dec :
           endif
       }⟩
     ⦃ y = x + z ⦄ := by
-  sorry
+  apply HIf
+  · apply HPreStrengthen
+    · apply HAsgn
+    verify_assertion
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
 
 def subtract_slowly {m p : ℕ} :
   ⊢ ⦃ ⊤ ⦄
@@ -79,7 +110,21 @@ def subtract_slowly {m p : ℕ} :
           od
       }⟩
     ⦃ z = p - m ⦄ := by
-  sorry
+  apply HSeq
+  · apply HSeq
+    · apply HPostWeaken
+      · apply HWhile ⦃ z - x = p - m ⦄
+        apply HSeq
+        · apply HAsgn
+        apply HPreStrengthen
+        · apply HAsgn
+        verify_assertion
+      verify_assertion
+    apply HAsgn
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
+
 
 def slow_assignment {m : ℕ} :
   ⊢ ⦃ "x" = m ⦄ -- ignore the apostrophes, fix is TODO for now, but meaning is as usual
@@ -91,7 +136,18 @@ def slow_assignment {m : ℕ} :
           od
       }⟩
     ⦃ "y" = m ⦄ := by
-  sorry
+  apply HSeq
+  · apply HPostWeaken
+    · apply HWhile ⦃ x + y = m ⦄
+      apply HSeq
+      · apply HAsgn
+      apply HPreStrengthen
+      · apply HAsgn
+      verify_assertion
+    verify_assertion
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
 
 
 def div_mod_dec {a b : ℕ} :
@@ -108,7 +164,25 @@ def div_mod_dec {a b : ℕ} :
   -- OPTIONAL (PR will pass without it)
   -- You may need the following helper lemmas:
   -- `natSumDiv`, `Nat.mod_eq_of_lt`
-  sorry
+  apply HSeq
+  · apply HSeq
+    · apply HPostWeaken
+      · apply HWhile ⦃ b * y + x = a ⦄
+        apply HSeq
+        · apply HAsgn
+        apply HPreStrengthen
+        · apply HAsgn
+        verify_assertion
+      verify_assertion
+      · rw [natSumDiv]
+        assumption
+      symm
+      apply Nat.mod_eq_of_lt
+      assumption
+    apply HAsgn
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
 
 def fib : ℕ → ℕ
   | 0 => 1
@@ -119,7 +193,12 @@ def fib : ℕ → ℕ
 
 lemma fib_eqn (n : ℕ) (h : n > 0) :
   fib n + fib (n - 1) = fib (1 + n) := by
-  sorry
+  cases n with
+  | zero => contradiction
+  | succ d =>
+    rw [Nat.add_one_sub_one]
+    nth_rewrite 3 [Nat.add_comm]
+    trivial
 
 def fibonacci {n f : ℕ} :
   ⊢ ⦃ ⊤ ⦄
@@ -138,4 +217,25 @@ def fibonacci {n f : ℕ} :
   -- OPTIONAL (PR will pass without it)
   -- You may need the following helper lemma:
   -- `fib_eqn`
-  sorry
+  apply HSeq
+  · apply HSeq
+    · apply HSeq
+      · apply HPostWeaken
+        · apply HWhile (fun σ => (σ "y" = fib (σ "x" - 1)) ∧ ((σ "z" = fib (σ "x"))) ∧ (σ "x" > 0))
+          apply HSeq
+          · apply HSeq
+            · apply HSeq
+              · apply HAsgn
+              apply HAsgn
+            apply HAsgn
+          apply HPreStrengthen
+          · apply HAsgn
+          verify_assertion
+          apply fib_eqn at a_2
+          exact a_2
+        verify_assertion
+      apply HAsgn
+    apply HAsgn
+  apply HPreStrengthen
+  · apply HAsgn
+  verify_assertion
