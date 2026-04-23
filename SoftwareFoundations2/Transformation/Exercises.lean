ab133bc690769c197da1e0a17af50b55c2db2cae
Cybot <stancilaionutmarian@yahoo.com>
2026-04-23 19:39:24 +0300

    Finished transformation exercises without CWhile

--- SoftwareFoundations2/Transformation/Exercises.lean
+++ SoftwareFoundations2/Transformation/Exercises.lean
@@ -27,20 +27,34 @@ theorem fold_constants_bexp_sound : BExp.fold_constants.sound := by
       rw [this]; clear this
       split <;> aesop
   | BNeq a₁ a₂  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [BExp.eval, BExp.fold_constants]
+      have : a₁.eval σ = a₁.fold_constants.eval σ := fold_constants_aexp_sound a₁ σ
+      rw [this]; clear this
+      have : a₂.eval σ = a₂.fold_constants.eval σ := fold_constants_aexp_sound a₂ σ
+      rw [this]; clear this
+      split <;> aesop
   | BLe a₁ a₂  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [BExp.eval, BExp.fold_constants]
+      have : a₁.eval σ = a₁.fold_constants.eval σ := fold_constants_aexp_sound a₁ σ
+      rw [this]; clear this
+      have : a₂.eval σ = a₂.fold_constants.eval σ := fold_constants_aexp_sound a₂ σ
+      rw [this]; clear this
+      split <;> aesop
   | BGt a₁ a₂  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [BExp.eval, BExp.fold_constants]
+      have : a₁.eval σ = a₁.fold_constants.eval σ := fold_constants_aexp_sound a₁ σ
+      rw [this]; clear this
+      have : a₂.eval σ = a₂.fold_constants.eval σ := fold_constants_aexp_sound a₂ σ
+      rw [this]; clear this
+      split <;> aesop
   | BNot b ih  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [BExp.eval, BExp.fold_constants]
+      rw [ih]
+      split <;> aesop
   | BAnd b₁ b₂ ih₁ ih₂  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [BExp.eval, BExp.fold_constants]
+      rw [ih₁, ih₂]
+      split <;> aesop
 
 open ComEval
 
@@ -49,14 +63,50 @@ theorem fold_constants_com_sound : Com.fold_constants.sound := by
   intro c σ₁ σ₂
   induction c generalizing σ₁ σ₂ with
   | CSkip       =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [Com.fold_constants]
   | CAsgn x a   =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+    simp only [Com.fold_constants]
+    apply Iff.intro
+    · -- x = a₁ -> x = a₂
+        intro h1
+        cases h1 with
+        | EAsgn eqn eqs =>
+            -- Guarantee that 'a' folds safely
+            have h_eq := fold_constants_aexp_sound a σ₁
+            -- Swap 'a' for 'a.fold_constants' in the evaluation proof
+            rw [h_eq] at eqn
+
+            apply EAsgn
+            · exact eqn -- updated evaluation proof
+            · exact eqs -- untouched state update proof
+    · -- x = a₂ -> x = a₁
+        intro h2
+        cases h2 with
+        | EAsgn eqn eqs =>
+            have h_eq := fold_constants_aexp_sound a σ₁
+            -- Rewrite backwards₁
+            rw [← h_eq] at eqn
+
+            apply EAsgn
+            · exact eqn
+            · exact eqs
   | CSeq c₁ c₂ h₁ h₂  =>
-      -- FILL IN HERE (optional: PR will pass without it)
-      sorry
+      simp only [Com.fold_constants]
+      apply Iff.intro
+      · intro hseq
+        cases hseq with
+        | ESeq hc1 hc2 =>
+            -- Build the new sequence
+            apply ESeq
+            · exact (h₁ _ _).mp hc1
+            · exact (h₂ _ _).mp hc2
+
+      · intro hseq
+        cases hseq with
+        | ESeq hc1 hc2 =>
+            apply ESeq
+            · exact (h₁ _ _).mpr hc1
+            · exact (h₂ _ _).mpr hc2
   | CIf b c₁ c₂ ih₁ ih₂ =>
       -- FILL IN HERE (optional: PR will pass without it)
       sorry
